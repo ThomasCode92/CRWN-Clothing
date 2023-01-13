6ad145506d19a4050ca72d552fea9faff7e35fe2
ThomasCode92 <thomascode92@hotmail.com>
2023-01-13 12:47:59 +0100

    React Router Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Route, Routes, Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
