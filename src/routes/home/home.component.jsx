889ba9b641f1bb1b25345ce0d3f90082fa3d2bdf
ThomasCode92 <thomascode92@hotmail.com>
2023-01-13 12:38:34 +0100

    Navigation Bar Component

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,3 @@
-import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 import categoriesData from '../../categories.json';
 
@@ -7,7 +6,6 @@ const Home = () => {
 
   return (
     <div>
-      <Outlet />
       <Directory categories={categories} />
     </div>
   );
