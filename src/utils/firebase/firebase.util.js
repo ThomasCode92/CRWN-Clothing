d051e1196ab5fdf13498dfafd34f8b462e5e397e
ThomasCode92 <thomascode92@hotmail.com>
2023-01-16 13:17:13 +0100

    Finish Creating User Documents

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -30,5 +30,16 @@ export const createUserDocumentFromAuth = async userAuth => {
   const userDocRef = doc(db, 'users', userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
 
-  console.log(userSnapshot.exists());
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
 };
