a5bf15d158e9d476252070e87aca742a498ee1d3
ThomasCode92 <thomascode92@hotmail.com>
2023-02-03 21:41:58 +0100

    Nested Routes in Shop

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment, useContext } from 'react';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+const CategoriesPreview = () => {
+  const categoriesMap = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map(categoryTitle => {
+        const products = categoriesMap[categoryTitle];
+        return (
+          <CategoryPreview
+            key={categoryTitle}
+            title={categoryTitle}
+            products={products}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
