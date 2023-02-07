19c10139ce24d7aff153f2a77d9ba9b7e5aec303
ThomasCode92 <thomascode92@hotmail.com>
2023-02-07 12:46:54 +0100

    Cart Reducer - Part 1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,12 @@
 import { createContext, useEffect, useState } from 'react';
 
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
 const addCartItem = (cartItems, item) => {
   const existingCartItem = cartItems.find(cartItem => cartItem.id === item.id);
 
@@ -32,6 +39,21 @@ const clearCartItem = (cartItems, item) => {
   return cartItems.filter(cartItem => cartItem.id !== item.id);
 };
 
+export const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: 'ADD_TO_CART',
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return { ...state, ...payload };
+    default:
+      throw new Error(`Unhandled type of ${type} in cartReducer!`);
+  }
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
@@ -61,6 +83,9 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
     setCartTotal(newCartTotal);
   }, [cartItems]);
+
+  const updateCartItems = newCartItems => {
+  };
 
   const addItemToCart = item => {
     setCartItems(prevState => addCartItem(prevState, item));
