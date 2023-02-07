72f137199e99adf69255a4c5193b8bc4ea1edbca
ThomasCode92 <thomascode92@hotmail.com>
2023-02-07 20:03:51 +0100

    Setting Up Our Store

--- src/store/store.js
+++ src/store/store.js
@@ -5,3 +5,9 @@ import {
 } from 'redux';
 import logger from 'redux-logger';
 
+import { rootReducer } from './root-reducer';
+
+const middlewares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
