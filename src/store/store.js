72dfbeb953394e665626994631783bcf0d6e202b
ThomasCode92 <thomascode92@hotmail.com>
2023-02-16 19:38:47 +0100

    Redux-Devtools

--- src/store/store.js
+++ src/store/store.js
@@ -9,8 +9,17 @@ import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 const persistConfig = {
   key: 'root',
