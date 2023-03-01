d741d050b2d88237c068d4b7e7b97601b8334d3c
ThomasCode92 <thomascode92@hotmail.com>
2023-03-01 07:47:59 +0100

    Redux-Thunk - Part 1

--- src/store/store.js
+++ src/store/store.js
@@ -6,12 +6,14 @@ import {
 import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
+const middlewares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
