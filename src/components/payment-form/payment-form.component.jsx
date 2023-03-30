8367fc80a4340658beb2b988463e5e4b2dcae600
ThomasCode92 <thomascode92@hotmail.com>
2023-03-30 07:59:16 +0200

    Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -7,10 +7,30 @@ const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
-  const paymentHandler = event => {
-    event.prefentDefault();
+  const paymentHandler = async event => {
+    event.preventDefault();
 
     if (!stripe || !elements) return;
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ amount: 10000 }),
+    });
+
+    const responseData = await response.json();
+    const clientSecret = responseData.paymentIntent.client_secret;
+
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: { name: 'Test User' },
+      },
+    });
+
+    if (paymentResult.error) return alert(paymentResult.error.message);
+
+    alert('Payment Successful');
   };
 
   return (
