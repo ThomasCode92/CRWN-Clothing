fb83b944f67884801345358dc3c7e9eb2587b2b7
ThomasCode92 <thomascode92@hotmail.com>
2023-05-03 11:03:41 +0200

    Typing Redux Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,21 +2,34 @@ import {
   applyMiddleware,
   compose,
   legacy_createStore as createStore,
+  Middleware,
 } from 'redux';
 import logger from 'redux-logger';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from '@redux-saga/core';
 
 import { rootReducer } from './root-reducer';
 import { rootSaga } from './root-saga';
 
+export type RootState = ReturnType<typeof rootReducer>;
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [
   process.env.NODE_ENV !== 'production' && logger,
   sagaMiddleware,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -26,7 +39,7 @@ const composeEnhancer =
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
-const persistConfig = {
+const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage: storage,
   whitelist: ['cart'],
