140e39ed0f44aa7034362a4bdf9c324c55d2a2a7
ThomasCode92 <thomascode92@hotmail.com>
2023-05-25 20:28:41 +0200

    Button Tests - Part 2

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -6,7 +6,28 @@ describe('button tests', () => {
   test('should render base button when nothing is passed', () => {
     render(<Button>Test</Button>);
 
-    const buttonElement = screen.getByRole('button', { name: /test/i });
+    const buttonElement = screen.getByRole('button');
     expect(buttonElement).toHaveClass('base');
+  });
+
+  test('should render google-sign-in button when passed google button type', () => {
+    render(<Button buttonType="google">Test</Button>);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toHaveClass('google-sign-in');
+  });
+
+  test('should render inverted button when passed inverted button type', () => {
+    render(<Button buttonType="inverted">Test</Button>);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toHaveClass('inverted');
+  });
+
+  test('should be disabled if isLoading is true', () => {
+    render(<Button isLoading>Test</Button>);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeDisabled();
   });
 });
