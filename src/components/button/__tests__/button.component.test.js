5dbd7328f2ea93cd43f8fa49167dba9226374d77
ThomasCode92 <thomascode92@hotmail.com>
2023-05-25 20:28:23 +0200

    Button Tests - Part 1

---
+++ src/components/button/__tests__/button.component.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import Button from '../button.component';
+
+describe('button tests', () => {
+  test('should render base button when nothing is passed', () => {
+    render(<Button>Test</Button>);
+
+    const buttonElement = screen.getByRole('button', { name: /test/i });
+    expect(buttonElement).toHaveClass('base');
+  });
+});
--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,11 +2,16 @@ import './button.styles.scss';
 import '../spinner/spinner.styles.scss';
 
 const BUTTON_TYPE_CLASSES = {
+  base: 'base',
   google: 'google-sign-in',
   inverted: 'inverted',
 };
 
 const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+  if (!buttonType) {
+    buttonType = 'base';
+  }
+
   return (
     <button
       className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
